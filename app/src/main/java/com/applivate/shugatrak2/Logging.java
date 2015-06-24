5dc2ba6c769f4448a9a02cf58d9b065b2486254c
Paul Sellards <paul.sellards@gmail.com>
2015-06-24 12:42:04 -0600

    fixed some warnings in Logging.arrayToDumpString()

--- app/src/main/java/com/applivate/shugatrak2/Logging.java
+++ app/src/main/java/com/applivate/shugatrak2/Logging.java
@@ -4,6 +4,9 @@ package com.applivate.shugatrak2;
 import android.util.Log;
 
 /**
+ *
+ * A Logging class to encapsulate logging functions.
+ *
  * Created by paul on 18/06/2015.
  */
 public class Logging {
@@ -71,16 +74,27 @@ public class Logging {
         Log.e(uniqueId, tag + ":  EXCEPTION: " + throwable.getMessage() + " - " + msg, throwable);
     }
 
+    /**
+     * This is a debugging tool.  It generates a printable string version of an array,
+     * with a title parameter to aid debugging.
+     * @param array The array to dump
+     * @param title A description of the array and the context it's being used in,
+     *              for aiding debugging.
+     * @return A string version of the contents of 'array'
+     */
     public static String arrayToDumpString(Object[] array, String title) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(title + "   ");
+        stringBuilder.append(title);
+        stringBuilder.append("   ");
         if (null == array) {
             stringBuilder.append("array is NULL");
         } else {
-            stringBuilder.append("array.length = " + array.length);
+            stringBuilder.append("array.length = ");
+            stringBuilder.append(array.length);
             for (int i = 0; i < array.length; i++) {
                 stringBuilder.append("\n");
-                stringBuilder.append(i + ": ");
+                stringBuilder.append(i);
+                stringBuilder.append(": ");
                 stringBuilder.append(String.valueOf(array[i]));
             }
         }
